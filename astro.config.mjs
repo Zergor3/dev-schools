76f9b1b6c7651c1cb89b326fe840cc5ca2f51837
Hector <zergor502@gmail.com>
2025-03-10 13:47:20 -0500

    github-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/dev-schools/",
+  site: 'https://Zergor3.github.io',
+  base: "dev-schools",
   vite: {
     plugins: [tailwindcss()]
   }
